1b223508c6973e6c8f72b3132a08bcc28ffe76b3
DevR <azammmgol@gmail.com>
2023-04-13 15:16:07 +0600

    Task 2: Add border around image which is selected

--- src/lib/mantine.ts
+++ src/lib/mantine.ts
@@ -3,8 +3,18 @@ import { MantineThemeOverride } from "@mantine/core";
 export const theme: MantineThemeOverride = {
   colorScheme: "dark",
   colors: {
-    brand: ['#F7DBF7', '#E4BFE4', '#D1A2D1', '#C17DC1', '#AC5DAB', '#782A77', '#651664', '#4C094B', '#3B053A', '#2A0229']
-    //          1          2          3          4           5   6(actual main)  main        8        9          10
+    brand: [
+      "#F7DBF7",
+      "#E4BFE4",
+      "#D1A2D1",
+      "#C17DC1",
+      "#AC5DAB",
+      "#782A77",
+      "#651664",
+      "#4C094B",
+      "#3B053A",
+      "#2A0229",
+    ],
   },
   primaryColor: "brand",
 };
