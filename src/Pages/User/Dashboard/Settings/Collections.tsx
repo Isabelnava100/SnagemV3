67b54a3bde2ba6c8c8123c3191b1fb4308c6e219
DevR <azammmgol@gmail.com>
2023-08-08 21:25:09 +0600

    WIP: Collections

--- src/Pages/User/Dashboard/Settings/Collections.tsx
+++ src/Pages/User/Dashboard/Settings/Collections.tsx
@@ -1,10 +1,15 @@
-import { Badge, Flex, Group, Image, Stack, Text, Title } from "@mantine/core";
+import { Anchor, Badge, Flex, Group, Image, List, Stack, Text, Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { Link } from "react-router-dom";
 import { SimpleSectionWrapper } from "../../../../components/Dashboard/SubTabsLayout";
 import { SectionLoader } from "../../../../components/navigation/loading";
 import { useAuth } from "../../../../context/AuthContext";
 import { ArrowSwapIcon } from "../../../../icons";
 import { getBadges, getEmojis } from "../../../../queries/settings";
+import Emoji1Src from "/src/assets/emojis/emoji-1.png";
+import Emoji2Src from "/src/assets/emojis/emoji-2.png";
+import Emoji3Src from "/src/assets/emojis/emoji-3.png";
 
 export type BadgeTypes = "New User" | "Admin" | "Legacy";
 
@@ -85,17 +90,163 @@ function Emojis() {
   const emojis = data;
   return (
     <SimpleSectionWrapper>
+      <Stack spacing={18}>
+        <Title size={24} color="white" weight={400} order={3}>
+          Your Emoji Collection
+        </Title>
+        <Flex wrap="wrap">
+          {emojis.map((emojiUrl: string) => (
+            <div key={emojiUrl}>
+              <Image width={30} sx={{ objectFit: "cover" }} src={emojiUrl} alt={emojiUrl} />
+            </div>
+          ))}
+        </Flex>
+      </Stack>
+    </SimpleSectionWrapper>
+  );
+}
+
+interface EmojiItem {
+  name: string;
+  description: string;
+  obtainingInstructions: React.ReactNode[];
+  imageSrc: string;
+}
+
+const emojiData: EmojiItem[] = [
+  {
+    name: "Happy Espeon",
+    description: "Commissioned from Seviyummy in 2022.",
+    imageSrc: Emoji1Src,
+    obtainingInstructions: [
+      <List.Item>Join before 2025.</List.Item>,
+      <List.Item>
+        Buy for 50 Snag Coins in the{" "}
+        <Anchor component={Link} to="/Shop">
+          Marketplace
+        </Anchor>
+        .
+      </List.Item>,
+      <List.Item>Buy for 1 Gem in the Credit Shop.</List.Item>,
+    ],
+  },
+  {
+    name: "Happy Espeon",
+    description: "Commissioned from Seviyummy in 2022.",
+    imageSrc: Emoji2Src,
+    obtainingInstructions: [
+      <List.Item>Join before 2025.</List.Item>,
+      <List.Item>
+        Buy for 50 Snag Coins in the{" "}
+        <Anchor component={Link} to="/Shop">
+          Marketplace
+        </Anchor>
+        .
+      </List.Item>,
+      <List.Item>Buy for 1 Gem in the Credit Shop.</List.Item>,
+    ],
+  },
+  {
+    name: "Zoroark's Laugh",
+    description: "Commissioned from Seviyummy in 2022.",
+    imageSrc: Emoji3Src,
+    obtainingInstructions: [
+      <List.Item>Join before 2025.</List.Item>,
+      <List.Item>
+        Buy for 50 Snag Coins in the{" "}
+        <Anchor component={Link} to="/Shop">
+          Marketplace
+        </Anchor>
+        .
+      </List.Item>,
+      <List.Item>Buy for 1 Gem in the Credit Shop.</List.Item>,
+    ],
+  },
+];
+
+function EmojiCollection() {
+  return (
+    <Stack spacing={18}>
       <Title size={24} color="white" weight={400} order={3}>
-        Your Emoji Collection
+        Collection of All Emojis
       </Title>
-      <Flex wrap="wrap">
-        {emojis.map((emojiUrl: string) => (
-          <div key={emojiUrl}>
-            <Image w={30} h={30} sx={{ objectFit: "cover" }} src={emojiUrl} alt={emojiUrl} />
-          </div>
+      <Flex wrap="wrap" gap={8}>
+        {emojiData.map((emoji, index) => (
+          <Flex
+            justify="center"
+            align="center"
+            key={index}
+            bg="#3C3A3C"
+            sx={{
+              borderRadius: "100%",
+              width: 50,
+              height: 50,
+              cursor: "pointer",
+              border: "3px solid",
+              borderColor: "transparent",
+            }}
+          >
+            <Image src={emoji.imageSrc} alt={emoji.name} width={30} height={30} />
+          </Flex>
         ))}
       </Flex>
-    </SimpleSectionWrapper>
+    </Stack>
+  );
+}
+
+interface BadgeItem {
+  name: string;
+  background: string;
+  description: string;
+}
+
+const badgeData: BadgeItem[] = [
+  {
+    name: "Legacy",
+    description: "“New User” Badges is enabled to anyone with less than 100 posts.",
+    background: "linear-gradient(90deg, #772976 0%, #464C9A 100%);",
+  },
+  {
+    name: "Admin",
+    description: "“Admin” Badges is enabled to anyone with less than 100 posts.",
+    background: "linear-gradient(90deg, #E54156 0%, #F59135 100%);",
+  },
+  {
+    name: "New User",
+    description: "“New User” Badges is enabled to anyone with less than 100 posts.",
+    background: "linear-gradient(90deg, #464C9A 0%, #469A95 100%);",
+  },
+];
+
+function BadgesCollection() {
+  return (
+    <Stack spacing={18}>
+      <Stack spacing={0}>
+        <Title size={24} color="white" weight={400} order={3}>
+          Collection of All Badges
+        </Title>
+        <Text>Here&apos;s a list of all badges and how to obtain them.</Text>
+      </Stack>
+      <Flex wrap="wrap" gap={8}>
+        {badgeData.map((badge, index) => (
+          <Badge
+            key={index}
+            size="lg"
+            sx={{
+              background: badge.background,
+              color: "white",
+              textTransform: "none",
+              fontSize: 16,
+              fontWeight: 400,
+              paddingTop: 5,
+              paddingBottom: 5,
+            }}
+          >
+            {badge.name}
+          </Badge>
+        ))}
+      </Flex>
+    </Stack>
   );
 }
 
@@ -103,21 +254,30 @@ function Collection() {
   return (
     <SimpleSectionWrapper>
       <Stack>
-        <h1>Collection of emojis</h1>
-        <h1>Collection of badges</h1>
+        <EmojiCollection />
+        <BadgesCollection />
       </Stack>
     </SimpleSectionWrapper>
   );
 }
 
 export default function Collections() {
+  const [showCollections, setShowCollections] = React.useState(false);
   return (
     <Stack w="100%">
       <SimpleSectionWrapper>
         <Badges />
       </SimpleSectionWrapper>
       <Emojis />
-      <Collection />
+      <Stack align="end">
+        <button
+          onClick={() => setShowCollections((pre) => !pre)}
+          className="text-[#E35C65] bg-transparent border-none outline-none text-[16px] font-[400] underline cursor-pointer"
+        >
+          {showCollections ? "Hide all collections" : "Show all collections"}
+        </button>
+      </Stack>
+      {showCollections && <Collection />}
     </Stack>
   );
 }
