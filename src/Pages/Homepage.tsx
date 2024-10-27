42283476fbb882faac94ad0a31cd6f0407b064e3
Isabel Nava <isabelnava100@gmail.com>
2024-10-27 20:07:42 -0300

    homepage hero

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,16 +1,27 @@
+import "/src/assets/styles/homepage.css";
+
 export const HomePage = () => {
   return (
-    <div style={{ position: "relative", display: "flex" }}>
-      <img
-        src="https://firebasestorage.googleapis.com/v0/b/snagemguild.appspot.com/o/edit_Bg_Compress.jpg?alt=media&token=69d117aa-15b7-4cee-868b-04b5835e87e4"
-        alt="mt fuji with pokemon"
-        style={{ maxWidth: "100%" }}
-      />
-      {/* 
-         Title: The Snagem Guild
-         A new imagined way of 
-             <i>story telling.</i> - A simple and easy to get started fantasy role-playing forums based on the Pokemon games.
-           */}
+    <div className="homepage">
+    <div className="heroIMGcontainer">
+           <div className="centered-container">
+              <h1>The Snagem Guild</h1>
+              <h2>Ready to start your journey?</h2>
+              <div className="button-container">
+              <a href="/" className="red button">Discover What We Do</a>
+              <a href="/Forum/Main-Forum" className="purple button">Go to the Forums</a>
+              </div>
+           </div>
+
+    </div>
+
+        <div className="secondmodule">
+      <h2>A new imagined way of <span>story telling.</span></h2>
+      <span>
+      It is simple and easy to get started in the role-playing, based on Pokemon.
+      </span>
+      <a href="/" className="button yellow">Read About Snagem</a>
+    </div>
     </div>
   );
 };
