57f1db55db36f4d810a020795c574ad679fcdfd9
Isabel Nava <isabelnava100@gmail.com>
2024-10-27 21:48:15 -0300

    second module

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -16,11 +16,23 @@ export const HomePage = () => {
     </div>
 
         <div className="secondmodule">
+          <div className="innerHold2 oneM">
       <h2>A new imagined way of <span>story telling.</span></h2>
-      <span>
+      <span className="paragraph2">
       It is simple and easy to get started in the role-playing, based on Pokemon.
       </span>
       <a href="/" className="button yellow">Read About Snagem</a>
+      </div>
+
+
+    <div className="innerHold2 twoM">
+      <h2 className="right-text">Forums custom built <span className="red-text">for roleplay.</span></h2>
+      <span className="paragraph2">
+      We created this platform that adjusts to our needs and regularly update it with new features.
+      <a href="/" className="button red">See Anticipated Updates</a>
+      </span>
+      
+      </div>
     </div>
     </div>
   );
